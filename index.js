987e7e1f214b1c2dcdfc2ca70b5170d5f11e4ed7
Jhefferson William <williamjhefferson6@gmail.com>
2020-11-02 11:23:40 -0300

    simple chat socket

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require("express")()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+
+app.use(require("express").static(__dirname + "/public"))
+app.use(require("express").json())
+app.use(require("express").urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname +  "/index.html")
+})
+
+app.post("/message", (req, res) => {
+    var message = req.body.message
+    io.emit("newmessage", message)
+})
+
+io.on('connection', (socket) => {
+    console.log("novo cliente conectado")
+
+    socket.on('disconnect', () => {
+        console.log("Este cliente desconectou")
+    })
+})
+
+http.listen(80, (err) => {
+    if(!err) console.log("Servidor iniciado com sucesso")
+    else  console.log("Erro ao iniciar servidor")
+})
